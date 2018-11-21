766ba7fe642301658063fce4d1f416a9e3122e10
Peter Rushforth <peter.rushforth@canada.ca>
2018-11-20 21:22:20 -0500

    Add hack to MapmlSerializer to write doctype once when clue is available; this should allow the modified TreeBuilder/HtmlParser to still work with HTML documents.

--- src/nu/validator/htmlparser/sax/MapmlSerializer.java
+++ src/nu/validator/htmlparser/sax/MapmlSerializer.java
@@ -3,13 +3,14 @@ package nu.validator.htmlparser.sax;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
+import org.xml.sax.Attributes;
 
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.ext.LexicalHandler;
 
 public class MapmlSerializer extends HtmlSerializer implements ContentHandler, LexicalHandler {
-    private static String doctype = "";
+    private static boolean doctypeWritten = false;
 
 
     public MapmlSerializer(OutputStream out) {
@@ -31,10 +32,27 @@ public class MapmlSerializer extends HtmlSerializer implements ContentHandler, L
           } else {
               writer.write("<!DOCTYPE html>\n");
           }
+          doctypeWritten = true;
         } catch (IOException e) {
             throw new SAXException(e);
         }
 
+    }
+    public void startElement(String uri, String localName, String qName,
+            Attributes atts) throws SAXException {
+        if (!doctypeWritten) {
+            try {
+              if (localName == "mapml") {
+                  writer.write("<!DOCTYPE mapml>\n");
+              } else {
+                  writer.write("<!DOCTYPE html>\n");
+              }
+              doctypeWritten = true;
+            } catch (IOException e) {
+                throw new SAXException(e);
+            }
+        }
+        super.startElement("http://www.w3.org/1999/xhtml", localName, qName, atts);
     }
 
 }
